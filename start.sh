776727bd8e20dc4f4e1c70a7f49cb4a78def3eb5
Marolok <bocharov_pavel@mail.ru>
2023-04-25 20:21:22 +0300

    Fix start.sh for start image profile.

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
-java -jar /opt/app/japp.jar \
-  --spring.profiles.active=image \
+java -jar -Dspring.profiles.active=image /opt/app/japp.jar \
   --spring.redis.host=${REDIS_HOST}  \
   --spring.redis.port=${REDIS_PORT} \
   --logging.file.path=${LOGGER_DIR}  \
